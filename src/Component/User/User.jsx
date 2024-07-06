ffe258ca38ab253632005e3a278c0ae6b2f418d3
razzak118348 <abdurrazzak118348@gmail.com>
2024-07-07 03:25:41 +0600

    another way to creat dynamic link with function

--- src/Component/User/User.jsx
+++ src/Component/User/User.jsx
@@ -1,9 +1,17 @@
 
 import PropTypes from 'prop-types'; 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const User = ({user}) => {
-    console.log(user)
+    // console.log(user)
+ 
     const {id,name, email,phone}=user;
+    const navigate = useNavigate();
+
+    const handleShowDetails =()=>{
+        navigate(`/user/${id}`);
+    }
+
+
     return (
         <div className='border-4 border-yellow-300 m-5 p-5 md:m-8 md:p-10 rounded-lg '>
             <h2>{id} :{name}</h2>
@@ -18,6 +26,10 @@ const User = ({user}) => {
            <Link to={`/user/${id}`}>
            <button className='p-1 border-y-amber-200 border-2 mx-3'>Click Me</button>
            </Link>
+
+           {/* another way */}
+           <button onClick={handleShowDetails} className='btn p-2 border-2 border-green-600 my-3'>User Details</button>
+
         </div>
     );
 };
