17ac4fa8d9c2a5bc7b8f70c94e7d638394e3b0f0
razzak118348 <abdurrazzak118348@gmail.com>
2024-07-07 03:52:03 +0600

    Link replace as NavLink

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,23 +1,23 @@
-import { Link } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="">
       <h2 className="text-xl font-medium mb-2">NavBar </h2>
-      <nav className="flex justify-center space-x-5 text-lg">
-        <Link className="hover:bg-black p-1 rounded-lg" to={"/"}>
+      <nav className="flex justify-center space-x-5 text-lg ">
+        <NavLink className="hover:bg-black p-1 rounded-lg" to={"/"}>
           Home
-        </Link>
-        <Link className="hover:bg-black p-1 rounded-lg" to={"/about"}>
+        </NavLink>
+        <NavLink className="hover:bg-black p-1 rounded-lg" to={"/about"}>
           About
-        </Link>
-        <Link className="hover:bg-black p-1 rounded-lg" to={"/contact"}>
+        </NavLink>
+        <NavLink className="hover:bg-black p-1 rounded-lg" to={"/contact"}>
           Contact Us
-        </Link>
-        <Link className="hover:bg-black p-1 rounded-lg" to={"/users"}>
+        </NavLink>
+        <NavLink className="hover:bg-black p-1 rounded-lg" to={"/users"}>
           Users
-        </Link>
+        </NavLink>
       </nav>
     </div>
   );
