77fb5039c4d25845c673440dea1b39cab3219d9b
razzak118348 <abdurrazzak118348@gmail.com>
2024-07-07 04:02:39 +0600

    useNavigation

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+const navigation =useNavigation();
+//useNavigation for loading data show 
+
   return (
     <div className="text-center">
         <h2 className="text-xl font-bold my-5">My website</h2>
+
       <Header></Header>
-      <Outlet></Outlet>
+      {
+        navigation.state === 'loading'? <button type="button " className="bg-indigo-500 ... p-1 rounded-lg mt-4" disabled>
+        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+        
+        </svg>
+        Processing...
+      </button> :   <Outlet></Outlet>
+      }
+   
       <Footer></Footer>
     </div>
   );
